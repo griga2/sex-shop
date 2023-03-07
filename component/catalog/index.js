015df35067fbe7f140aa4d0d50e677363dd20bcb
griga2 <griga2999wow@gmail.com>
2023-03-08 01:27:27 +0700

    fglerjhoe[rjhtpodkfhdrfhdertyhonweroyhn[enhy

---
+++ component/catalog/index.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString(),
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    },
+    function:{
+
+    }
+})
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function getHyi() {
     return "/t/t[]/n/--------/n\--------/t/t[]"
 }
+
+function draf_table_footer(mas, div_class){
+    let div_for_input = document.querySelector(`div.${div_class}`);
+    for ( i = 0 ; i<mas.length;i++) { 
+        let obj = document.createElement("a");
+        obj.getAttribute("class ","flex_item");
+        obj.textContent = mas_categ[i];
+        div_for_input.appendChild(obj);
+    }
+    console.log(mas.length)
+}
